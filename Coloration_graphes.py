d9198aec824a078473d0d35d67241b53916ddf8b
Matthieu Speismann <matt.speismann@gmail.com>
2024-01-16 14:29:36 +0100

    Documentation terminée
    Traduction de tout en anglais et terminer la documentation.

--- Coloration_graphes.py
+++ Coloration_graphes.py
@@ -12,8 +12,9 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 # Variables globales / Global variables:
-#  Celles-ci sont utile pour tester ici / Useful for tests here 
+# Celles-ci sont utile pour tester ici / Useful for tests here 
 # Dans le fichier principal, elle sont obtenues à partir des données / In the main file, the are deducted from data.
+
 availability_courses: list[list[int]] = [
             [1, 0, 1, 0, 1],
             [0, 1, 0, 1, 0],
@@ -30,6 +31,7 @@ availability_courses: list[list[int]] = [
     list[list[int]]: Cours indicées en lignes et créneaux indicées en lignes / Courses indexed in ligns and time in columns.
 """
 
+
 number_rooms: int = 2
 """Entier représentant le nombre de classe disponible dans l'école.
 -
@@ -38,11 +40,13 @@ Integer representing the amount of rooms available.
 Le nombre de cours ayant lieu sur un même créneau ne peut alors pas être supérieur à ce nombre.
 In consequences, the number of courses taking place at the same time cannot bea bove that number."""
 
+
 colors: list[str] = ['b','g','r','y','m','c','k','w']
 """ Cette liste permet d'obtenir une couleur (représentée par une chaine de caractères) à partir de son index
 
     This list allows to get a color (represented by a string) thanks to its index.
 """
+
 
 colors_dict: dict[str, int] = {}
 """ Ce dictionnaire permet d'obtenir l'indice d'une couleur à partir de sa chaine de caractère.
@@ -59,11 +63,10 @@ def normalisation(graph):
     -
     Function to normalise the graph for the functions we are using.
     - 
-    Modifie le graph en place pour que les nodes soit triés dans son encodage.
-    
-    
+    Modifie le graph en place pour que les nodes soit triés dans son encodage / Modify the graph in place in order to sort the nodes.     
+
     Args:
-        graph (Graph)
+        graph (Graph): Graphe à normaliser / Graph to normalize. 
     """
 
     nodes = list(graph.nodes)
@@ -76,48 +79,70 @@ def normalisation(graph):
     for (node1, node2) in edges:
         graph.add_edge(node1, node2)
 
-    
+
 def trivial_coloring(graph) -> list[str]:
-    """Propose une coloration triviale.
+    """ Propose une coloration triviale.
     -
-    A chaque noeud est associée une couleur différente. 
+    Get a trivial coloration.
+    -
+    A chaque noeud est associée une couleur différente / To each node is associated a different color
 
     Args:
-        graph (Graph): Graphe non orienté [Networkx]
+        - graph (Graph): Graphe non orienté [Networkx] / Unoriented graph [Networkx]
 
     Returns:
-        list[str]: Liste des couleurs pour chaque noeuds.
+        - list[str]: Liste des couleurs pour chaque noeuds./ List of the color for each node. 
     """
 
     node_colors: list[str] = []
     for node in graph.nodes():
         for index in range(len(colors)):
             # Vérification que la couleur est différente de toutes les autres:
+            # Verification that the color is different form the others:
             if colors[index] not in node_colors:
                 # Vérification que le prof est disponible:
+                # Verification that the teacher is available:
                 try:
                     if availability_courses[node][index] == 1:
                         node_colors.append(colors[index])
                         break
                 # Si l'erreur a lieu, cela signifie qu'il n'y a pas assez de couleur (et donc de créneaux) pour avoir une solution acceptable.
+                # If the error take place, it means that there is not enough colors (and so of times) to have an acceptable solution. 
                 except IndexError:
                     raise ValueError("No available solution with this method.")
     return node_colors
 
 def number_courses_color(color: str, node_colors: list[str]) -> int:
-    """ """ 
+    """Récupère le nombre de cours qui ont déjà cette couleur.
+    -
+    Get the number of courses which are already on the input color
+    -
+
+    Args:
+        - color (str): Chaine de caractère qui représente la couleur : string that represents the color. 
+        - node_colors (list[str]): Liste des noeuds de couleur actuel / Liste of the node colors at this time.
+
+    Returns:
+        - int: Nombre de cours ayant déjà cette couleur / Number of courses which have already this input color.  
+    """
     res = 0
     for elem in node_colors:
         if elem == color:
             res += 1
     return res
 
+
 def greedy_coloring(graph) -> list[str]:
     """Algoritme glouton de coloration d'un graphe
     -
-    Nous allons envisager tous les sommets successivement et attribuer à un sommet la plus petite couleur possible parmi celles déjà utilisées
+    Graph coloration greedy algorithm
+    - 
+    En parcourant tous les sommets successivement et attribuer à un sommet la plus petite couleur possible parmi celles déjà utilisées
     (différente de celles de ses voisins s'ils sont déjà colorés) à ce sommet, ou bien une nouvelle couleur si toutes les couleurs 
     sont utilisées par les voisins.
+    
+    By using every nodes successively and attributing for a node the tinier color possible which is different for all its neighbors
+    to its node or a new color if they are all already used by its neighbors.
 
     Args:
         graph (Graph): Graphe non orienté [Networkx]
@@ -127,30 +152,30 @@ def greedy_coloring(graph) -> list[str]:
     """
 
     node_colors: list[str] = []
-    # Parcours de tous les sommets du graphes:
+    # Parcours de tous les sommets du graphes / Browsing all nodes of the graph:
     for sommet in graph.nodes():
-        neighbors = list(graph.neighbors(sommet)) # Voisins de ce sommets
+        neighbors = list(graph.neighbors(sommet)) # Voisins de ce sommets / Neighbors of the node:
         neighbors_colors = [] 
-        # Parcours des voisins du sommet:
+        # Parcours des voisins du sommet/ Browsing the neighbors:
         for neighbor in neighbors:
             try:
-                # Ajouter la couleurs du voisins dans une liste
+                # Ajouter la couleurs du voisins dans une liste/ Adding the color of the neighbor to a list: 
                 neighbors_colors.append(node_colors[neighbor])
             except IndexError:
-                pass # Si la couleur n'est pas encore attribuée, ignorer
-        # Pour toutes les couleurs définies:
+                pass # Si la couleur n'est pas encore attribuée, ignorer / If not allready attibuted, pass:
         for color in colors:
-            # Si la couleurs n'est pas parmi les voisins:
+            # Si la couleurs n'est pas parmi les voisins / If the color is not in the neighbors: 
             if color not in neighbors_colors:
                 index = colors_dict[color]
-                # Vérification de la diponibilité du prof:
+                # Vérification de la diponibilité du prof / Teacher availability verification: 
                 try:
                     if availability_courses[sommet][index] == 1:
-                        # Vérification de la condition sur le nombre de salles de classe:
+                        # Vérification de la condition sur le nombre de salles de classe / Number of rooms availability verification:
                         if number_courses_color(color, node_colors) < number_rooms:
                             node_colors.append(colors[index])
                             break
                 # Si l'erreur a lieu, cela signifie qu'il n'y a pas assez de couleur (et donc de créneaux) pour avoir une solution acceptable.
+                # If the error take place, it means that there is not enough colors (and so times) to construct an available solution:
                 except IndexError:
                     raise ValueError("No available solution with this method.")
     return node_colors
@@ -158,12 +183,13 @@ def greedy_coloring(graph) -> list[str]:
 def degree_order(graph):
     """ Retourne les nœuds triés par ordre décroissant de degré
     - 
-    
+    Returns the nodes sorted by decreasing degree:
+
     Args:
-        graph (Graphe): Graphe Networkx 
+        graph (Graphe): Graphe Networkx / Graph Networkx
 
     Returns:
-        list[int]: _description_
+        list[int]: Liste des sommets triées par degeré décroissan / List of the nodes ordered by decreasing degree.
     """
 
     return sorted(graph.nodes(), key=lambda x: graph.degree[x], reverse=True)
@@ -181,31 +207,31 @@ def improved_greedy_coloring(graph) -> list[str]:
     """
 
     node_colors: list[str] = []
-    # Parcours de tous les sommets du graphes:
     nodes = degree_order(graph)
-    for node in nodes:
-        neighbors = list(graph.neighbors(node)) # Voisins de ce sommets
+    # Parcours de tous les sommets du graphes / Browsing all nodes of the graph:
+    for sommet in graph.nodes():
+        neighbors = list(graph.neighbors(sommet)) # Voisins de ce sommets / Neighbors of the node:
         neighbors_colors = [] 
-        # Parcours des voisins du sommet:
+        # Parcours des voisins du sommet/ Browsing the neighbors:
         for neighbor in neighbors:
             try:
-                # Ajouter la couleurs du voisins dans une liste
+                # Ajouter la couleurs du voisins dans une liste/ Adding the color of the neighbor to a list: 
                 neighbors_colors.append(node_colors[neighbor])
             except IndexError:
-                pass # Si la couleur n'est pas encore attribuée, ignorer
-        # Pour toutes les couleurs définies:
+                pass # Si la couleur n'est pas encore attribuée, ignorer / If not allready attibuted, pass:
         for color in colors:
-            # Si la couleurs n'est pas parmi les voisins:
+            # Si la couleurs n'est pas parmi les voisins / If the color is not in the neighbors: 
             if color not in neighbors_colors:
                 index = colors_dict[color]
-                # Vérification de la diponibilité du prof:
+                # Vérification de la diponibilité du prof / Teacher availability verification: 
                 try:
-                    if availability_courses[node][index] == 1:
-                        # Vérification de la condition sur le nombre de salles de classe:
+                    if availability_courses[sommet][index] == 1:
+                        # Vérification de la condition sur le nombre de salles de classe / Number of rooms availability verification:
                         if number_courses_color(color, node_colors) < number_rooms:
                             node_colors.append(colors[index])
                             break
                 # Si l'erreur a lieu, cela signifie qu'il n'y a pas assez de couleur (et donc de créneaux) pour avoir une solution acceptable.
+                # If the error take place, it means that there is not enough colors (and so times) to construct an available solution:
                 except IndexError:
                     raise ValueError("No available solution with this method.")
     return node_colors
